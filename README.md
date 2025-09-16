7183c61e2cb2077877e5b0cfdaf0f391e044226c
Harsh Kumar <harshjangir8586@gmail.com>
2025-09-16 10:59:36 +0530

    Second Commit after adding some basic JS features.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# It's my second project with HTML and CSS only.
+# It's my second project with HTML and CSS and JavaScript.
 ## It's fully responsive for most of the screen sizes.
-## No logic for now (will add in future as I acquire new skills.).
+## I have added some basic JS features.
+### Please keep in mind that it's my very second project only, and hence, is not the best and full of features like the original page.
+### But I guess it's good and I am happy with my work in this project. 
 [Live Demo](https://harsmeow.github.io/Project-2-SpaceX-Mission-Mars-Landing-Page-Clone/)
---
+++ script.js
@@ -0,0 +1,59 @@
+document.getElementsByClassName('explore_button')[0].addEventListener('click', function() {
+    window.location.href = 'https://www.spacex.com/humanspaceflight/mars';
+});
+
+document.getElementById('starship_btn').addEventListener('click', function() {
+    window.location.href = 'https://www.spacex.com/vehicles/starship';
+});
+
+document.getElementsByClassName('section10_button')[0].addEventListener('click', function() {
+    window.location.href = 'https://www.starlink.com/';
+});
+
+document.getElementsByClassName('x_logo_button')[0].addEventListener('click', function() {
+    window.location.href = 'https://x.com/SpaceX';
+});
+
+document.getElementById('footer_item_1').addEventListener('click', function() {
+    window.location.href = 'https://www.spacex.com/careers';
+});
+
+document.getElementById('footer_item_2').addEventListener('click', function() {
+    window.location.href = 'https://www.spacex.com/updates';
+});
+
+document.getElementById('footer_item_3').addEventListener('click', function() {
+    window.location.href = 'https://www.spacex.com/assets/media/privacy_policy_spacex.pdf';
+});
+
+document.getElementById('footer_item_4').addEventListener('click', function() {
+    window.location.href = 'https://www.spacex.com/supplier';
+});
+
+document.getElementById('nav_item2').addEventListener('click', function() {
+    window.location.href = 'https://www.spacex.com/launches';
+});
+
+document.getElementById('nav_item3').addEventListener('click', function() {
+    window.location.href = 'https://www.spacex.com/humanspaceflight';
+});
+
+document.getElementById('nav_item4').addEventListener('click', function() {
+    window.location.href = 'https://www.spacex.com/rideshare';
+});
+
+document.getElementById('nav_item5').addEventListener('click', function() {
+    window.location.href = 'https://www.starlink.com/';
+});
+
+document.getElementById('nav_item6').addEventListener('click', function() {
+    window.location.href = 'https://www.spacex.com/starshield';
+});
+
+document.getElementById('nav_item8').addEventListener('click', function() {
+    window.location.href = 'https://shop.spacex.com/';
+});
+
+document.getElementsByClassName('spacex_svg')[0].addEventListener('click', function() {
+    window.location.href = 'https://www.spacex.com/';
+});
